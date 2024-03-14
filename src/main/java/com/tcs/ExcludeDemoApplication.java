792f330716c808c925b293bbb715416ae484ca1f
javashashavali <javashashavali@gmail.com>
2024-03-14 16:19:26 +0530

    Exclude properties

---
+++ src/main/java/com/tcs/ExcludeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcludeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcludeDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/tcs/entity/Task.java
@@ -0,0 +1,15 @@
+package com.tcs.entity;
+
+public class Task {
+	
+	
+	
+	private Integer taskId;
+	
+	private String taskName;
+	
+	private String  duration;
+	
+	private String backlogs;
+
+}
